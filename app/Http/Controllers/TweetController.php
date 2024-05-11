831af077869136da8a8fade518fd40f39af9ebfd
K-Akira928 <K.AkiraRei928@gmail.com>
2024-05-11 15:22:30 +0900

    feat: ツイート作成処理を実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Requests\TweetRequest;
+
+use App\Models\User;
+use App\Models\Tweet;
 
 class TweetController extends Controller
 {
     public function index(): View
     {
         return view('home');
+    }
+
+    public function store(TweetRequest $request)
+    {
+        $tweet = new Tweet(['content' => $request->content]);
+        Auth::user()->tweets()->save($tweet);
+
+        return redirect()->route('home');
     }
 }
