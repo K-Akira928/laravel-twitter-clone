45bfb31dbe88b6b3d83802e405001dad27790048
K-Akira928 <K.AkiraRei928@gmail.com>
2024-05-15 14:22:32 +0900

    feat: User と UserIcon のリレーションを設定

--- app/Models/UserIcon.php
+++ app/Models/UserIcon.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
 
 class UserIcon extends Model
 {
@@ -15,4 +16,8 @@ class UserIcon extends Model
         'filename'
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
